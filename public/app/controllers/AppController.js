6361680ef4ec785f44e9338f330bf29387672b69
Jean-David Flament <flamentjeandavid@yahoo.fr>
2017-12-14 13:41:57 +0100

    remove form to create and delete currency

--- public/app/controllers/AppController.js
+++ public/app/controllers/AppController.js
@@ -4,30 +4,6 @@ function AppController($scope, currenciesFactory, $location, $http) {
 
     $scope.currenciesFactory = currenciesFactory;
 
-    // when submitting the add form, send the text to the node API
-    $scope.createCurrency = function() {
-        $http.post('/api/currencies', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.currencies = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-
-    // delete a currency after checking it
-    $scope.deleteCurrency = function(id) {
-        $http.delete('/api/currencies/' + id).success(function(data) {
-            $scope.currencies = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-    };
-
     currenciesFactory.getCurrencies(function(data) {
         $scope.currencies = data.Data;
         console.log($scope.currencies);
