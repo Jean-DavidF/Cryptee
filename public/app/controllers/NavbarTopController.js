e5b3f5f01acf46c7274dc2fed619ce36d31086e7
Jean-David Flament <flamentjeandavid@yahoo.fr>
2017-12-19 14:50:08 +0100

    convert EUR in cryptopage and isActive dashboard

--- public/app/controllers/NavbarTopController.js
+++ public/app/controllers/NavbarTopController.js
@@ -3,6 +3,10 @@ function NavbarTopController($scope, $location) {
 	$scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+
+    $scope.isDashboard = function (viewLocation) {
+		return $location.path().indexOf(viewLocation) !== -1;
+    }
 }
 
 crypteeApp.controller('NavbarTopController', ['$scope', '$location', NavbarTopController]);
