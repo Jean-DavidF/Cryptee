06b69b40682e33ddf6cb3314e4f4ebd35e34833e
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-01-11 09:48:12 +0100

    update app controller

--- public/app/controllers/AppController.js
+++ public/app/controllers/AppController.js
@@ -1,36 +1,26 @@
-function AppController($scope, currenciesFactory, $location, $http) {
-    $scope.formData = {};
+function AppController($scope, currenciesFactory, $location, $http, $state) {
     $scope.currencies = [ ];
+    $scope.searchCurrency = '';
 
     $scope.currenciesFactory = currenciesFactory;
-
-    // when submitting the add form, send the text to the node API
-    $scope.createCurrency = function() {
-        $http.post('/api/currencies', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.currencies = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-
-    // delete a currency after checking it
-    $scope.deleteCurrency = function(id) {
-        $http.delete('/api/currencies/' + id).success(function(data) {
-            $scope.currencies = data;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
-    };
 
     currenciesFactory.getCurrencies(function(data) {
         $scope.currencies = data;
     });
+
+    var AllCurrenciesInterval = setInterval(function(){
+        if ('/dashboard' === $location.path()) {
+            currenciesFactory.getCurrencies(function(data) {
+                $scope.currencies = data;
+            });
+        } else {
+            clearInterval(AllCurrenciesInterval);
+        }
+    }, 60000);
+
+    $scope.viewCurrency = function(Id) {
+        $state.go('currency', {CoinId : Id});
+    };
 }
 
-crypteeApp.controller('AppController', ['$scope', 'currenciesFactory', '$location', '$http', AppController]);
+crypteeApp.controller('AppController', ['$scope', 'currenciesFactory', '$location', '$http', '$state', AppController]);
