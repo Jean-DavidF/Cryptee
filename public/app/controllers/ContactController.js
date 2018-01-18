5df4e3245a29fed6bbf2604ddf4ca75586a71657
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-01-18 11:52:11 +0100

    submit form test

--- public/app/controllers/ContactController.js
+++ public/app/controllers/ContactController.js
@@ -1,4 +1,7 @@
 function ContactController($scope) {
+	$scope.sendmail = function() {
+        console.log('form submitted');
+    };
 }
 
 crypteeApp.controller('ContactController', ['$scope', ContactController]);
