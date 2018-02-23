f4844446ddebe565f5974a29bc37e08eb1d92226
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-02-23 11:24:40 +0100

    navbar top condition

--- public/app/controllers/NavbarTopController.js
+++ public/app/controllers/NavbarTopController.js
@@ -1,7 +1,15 @@
 function NavbarTopController($scope, $location) {
 
     $scope.isActive = function (viewLocation) {
-		return $location.path().indexOf(viewLocation) !== -1;
+		    return $location.path().indexOf(viewLocation) !== -1;
+    }
+
+    $scope.isDashboard = function () {
+        if ($location.path().indexOf('/dashboard') !== -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
--- public/app/controllers/SidebarController.js
+++ public/app/controllers/SidebarController.js
@@ -1,8 +1,4 @@
 function SidebarController($scope, $location) {
-
-    $scope.isActive = function (viewLocation) {
-		    return $location.path().indexOf(viewLocation) !== -1;
-    }
 }
 
 crypteeApp.controller('SidebarController', ['$scope', '$location', SidebarController]);
