f13f8fec3867b9acfb8ecde53dc4b3c76f6df321
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-05 11:43:29 +0200

    bigger sidebar and landing crypto link

--- public/app/controllers/LandingController.js
+++ public/app/controllers/LandingController.js
@@ -1,9 +1,13 @@
-function LandingController($scope, currenciesFactory) {
+function LandingController($scope, currenciesFactory, $state) {
   $scope.currenciesFactory = currenciesFactory;
 
   currenciesFactory.getTwoFirst(function(data) {
       $scope.currencies = data;
   });
+
+  $scope.viewCurrency = function(Id) {
+      $state.go('currency', {CoinId : Id});
+  };
 }
 
-crypteeApp.controller('LandingController', ['$scope', 'currenciesFactory', LandingController]);
+crypteeApp.controller('LandingController', ['$scope', 'currenciesFactory', '$state', LandingController]);
