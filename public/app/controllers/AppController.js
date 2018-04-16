5ba2905ab2a5eeff436a2c42dfbe9bad98dbba44
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-16 17:11:26 +0200

    remove navbar dashboard and wip filters

--- public/app/controllers/AppController.js
+++ public/app/controllers/AppController.js
@@ -8,6 +8,37 @@ function AppController($scope, currenciesFactory, $location, $http, $state) {
         $scope.currencies = data;
     });
 
+    var sort_by = function(field, reverse, primer){
+
+       var key = primer ?
+           function(x) {return primer(x[field])} :
+           function(x) {return x[field]};
+
+       reverse = !reverse ? 1 : -1;
+
+       return function (a, b) {
+           return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+         }
+    }
+
+    $scope.updateChange = function() {
+			if ($scope.currencies) {
+        $scope.currencies.sort(sort_by($scope.selectChange, true));
+      }
+		};
+
+    $scope.updateValue = function() {
+      var parameter = 'cmc_price_usd';
+      var mapping = {
+        'ascending' : true,
+        'descanding' : false
+      }
+
+      if ($scope.currencies) {
+        $scope.currencies.sort(sort_by(parameter, mapping[$scope.selectValue], parseInt));
+      }
+    }
+
     var AllCurrenciesInterval = setInterval(function(){
         if ('/dashboard' === $location.path()) {
             currenciesFactory.getCurrencies(function(data) {
