5ae76c5a06c4c4e72a4712ada7be88e604ae19c6
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-04-19 20:14:02 +0200

    wip filters

--- public/app/controllers/AppController.js
+++ public/app/controllers/AppController.js
@@ -1,5 +1,6 @@
 function AppController($scope, currenciesFactory, $location, $http, $state) {
     $scope.currencies = [ ];
+    $scope.saveCurrencies = [ ];
     $scope.searchCurrency = '';
 
     $scope.currenciesFactory = currenciesFactory;
@@ -7,6 +8,8 @@ function AppController($scope, currenciesFactory, $location, $http, $state) {
     currenciesFactory.getCurrencies(function(data) {
         $scope.currencies = data;
     });
+
+    $scope.dateTime = new Date;
 
     var sort_by = function(field, reverse, primer){
 
@@ -22,9 +25,16 @@ function AppController($scope, currenciesFactory, $location, $http, $state) {
     }
 
     $scope.updateChange = function() {
-			if ($scope.currencies) {
-        $scope.currencies.sort(sort_by($scope.selectChange, true));
+      if ($scope.selectChange) {
+        if ($scope.currencies) {
+          $scope.currencies.sort(sort_by($scope.selectChange, true));
+        }
+      } else {
+        currenciesFactory.getCurrencies(function(data) {
+            $scope.currencies = data;
+        });
       }
+
 		};
 
     $scope.updateValue = function() {
@@ -34,16 +44,38 @@ function AppController($scope, currenciesFactory, $location, $http, $state) {
         'descanding' : false
       }
 
-      if ($scope.currencies) {
-        $scope.currencies.sort(sort_by(parameter, mapping[$scope.selectValue], parseInt));
+      if ($scope.selectValue) {
+        if ($scope.currencies) {
+          $scope.currencies.sort(sort_by(parameter, mapping[$scope.selectValue], parseInt));
+        }
+      } else {
+        currenciesFactory.getCurrencies(function(data) {
+            $scope.currencies = data;
+        });
       }
+
     }
 
     var AllCurrenciesInterval = setInterval(function(){
         if ('/dashboard' === $location.path()) {
+          console.log($scope.selectValue);
+          console.log($scope.selectChange);
+          if ($scope.selectValue == undefined && $scope.selectChange == undefined) {
             currenciesFactory.getCurrencies(function(data) {
                 $scope.currencies = data;
             });
+          } else if ($scope.selectValue !== undefined) {
+            currenciesFactory.getCurrencies(function(data) {
+                $scope.currencies = data;
+                $scope.updateValue();
+            });
+
+          } else if ($scope.selectChange !== undefined) {
+            currenciesFactory.getCurrencies(function(data) {
+                $scope.currencies = data;
+                $scope.updateChange();
+            });
+          }
         } else {
             clearInterval(AllCurrenciesInterval);
         }
@@ -52,6 +84,35 @@ function AppController($scope, currenciesFactory, $location, $http, $state) {
     $scope.viewCurrency = function(Id) {
         $state.go('currency', {CoinId : Id});
     };
+
+    Number.prototype.padLeft = function(base,chr){
+       var  len = (String(base || 10).length - String(this).length)+1;
+       return len > 0? new Array(len).join(chr || '0')+this : this;
+    }
+
+
+    var d = new Date,
+        dformat = [ d.getDate().padLeft(),
+                    (d.getMonth()+1).padLeft(),
+                    d.getFullYear()].join('/')+
+                    ' à ' +
+                  [ d.getHours().padLeft(),
+                    d.getMinutes().padLeft(),
+                  ].join('h');
+     $scope.dateTime = dformat;
+
+     function resetFilter(select1, select2) {
+       $(document).on('change', 'select[name=' + select1 + ']', function(event) {
+         var select = $('select[name=' + select2 + ']');
+         if (select.val()) {
+           select.val('');
+         }
+       });
+     }
+
+     resetFilter('selectChange', 'selectValue');
+     resetFilter('selectValue', 'selectChange');
+
 }
 
 crypteeApp.controller('AppController', ['$scope', 'currenciesFactory', '$location', '$http', '$state', AppController]);
