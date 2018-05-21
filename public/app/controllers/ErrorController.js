cecf6b23b6b31ee8060ca414429d80411db30c45
Jean-David Flament <flamentjeandavid@yahoo.fr>
2018-05-21 17:52:29 +0200

    fix error page redirect

--- public/app/controllers/ErrorController.js
+++ public/app/controllers/ErrorController.js
@@ -1,4 +1,19 @@
-function ErrorController($scope) {
+function ErrorController($scope, $rootScope, $state) {
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+      $rootScope.previousState = {state: fromState, params: fromParams};
+  });
+
+  $rootScope.backState = function(state, params) {
+      if (state == undefined || state == null) {
+        $state.go('landing');
+        return;
+      }
+      if (params && Object.keys(params) == 'CoinId') {
+        $state.go('dashboard');
+        return;
+      }
+      $state.go(state.name, params);
+  };
 }
 
-crypteeApp.controller('ErrorController', ['$scope', ErrorController]);
+crypteeApp.controller('ErrorController', ['$scope', '$rootScope', '$state', ErrorController]);
